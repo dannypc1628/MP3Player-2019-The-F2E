3c39e285ead8a37f7bde4876d7c8b936b393e12e
dannyLiu <h152074716@gmail.com>
2019-08-05 10:29:57 +0800

    增加播放音樂功能

--- js/week3.js
+++ js/week3.js
@@ -1,2 +1,25 @@
-var audio = document.getElementById("Music");
+var MyAudio = document.getElementById("Music");
 //audio.play();
+
+var app = new Vue({
+    el:"#player",
+    data:{
+        MusicIsPaused:true
+    },
+    methods:{
+        PlayOrPaused:function(){
+            this.MusicIsPaused=!this.MusicIsPaused;
+            this.MusicStatus();
+        },
+        MusicStatus:function(){
+            if(this.MusicIsPaused){
+                MyAudio.pause();
+            }
+            else{
+                MyAudio.play(); 
+            }
+        }
+    },
+    conputed:{}
+
+});
